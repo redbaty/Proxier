625d84f322714a7b23e5a7c45d70e15ac04db9a3
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-05-03 17:59:33 -0300

    Rework start
    * Based on CSharp Analyzer and roslyn compiler

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using Proxier.Builders;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public string Hellow { get; }
+
+        static void Main(string[] args)
+        {
+            var x = new ClassBuilder();
+            var result = x.FromType(typeof(Program)).Build().Result;
+        }
+    }
+}
