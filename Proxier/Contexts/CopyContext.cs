a465896cd3986738228e51d98ab066c231054c3f
redbaty <juninhowblu@gmail.com>
2018-05-04 09:12:01 -0300

    Added CopyContext

---
+++ Proxier/Contexts/CopyContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Proxier.Interfaces;
+
+namespace Proxier.Extensions
+{
+    internal class CopyContext : ICopyContext
+    {
+        /// <inheritdoc />
+        public PropertyInfo Property { get; }
+
+        /// <inheritdoc />
+        public object Source { get; }
+
+        /// <inheritdoc />
+        public object Target { get; }
+
+        public CopyContext(PropertyInfo property, object source, object target)
+        {
+            Property = property;
+            Source = source;
+            Target = target;
+        }
+    }
+}
