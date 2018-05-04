1b97801f8f49faae5a1e32d4f0d860ae18da1e8c
redbaty <juninhowblu@gmail.com>
2018-05-04 09:23:35 -0300

    Added docs to CopyToOptions

--- Proxier/CopyToOptions.cs
+++ Proxier/CopyToOptions.cs
@@ -3,12 +3,27 @@ using Proxier.Interfaces;
 
 namespace Proxier.Extensions
 {
+    /// <summary>
+    /// Represents copy options
+    /// </summary>
     public class CopyToOptions
     {
+        /// <summary>
+        /// If set, the copier will include non public properties.
+        /// </summary>
+        /// <returns></returns>
         public bool CopyPrivates { get; set; }
 
+        /// <summary>
+        /// If set this will not copy null values.
+        /// </summary>
+        /// <returns></returns>
         public bool IgnoreNulls { get; set; }
 
+        /// <summary>
+        /// This will be used to get the value that will be set to a certain property.
+        /// </summary>
+        /// <returns></returns>
         public Func<ICopyContext, object> Resolver { get; set; } = context => context.Property.GetValue(context.Source);
     }
 }
