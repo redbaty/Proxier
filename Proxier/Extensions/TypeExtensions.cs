2b0679dc9dd893715197a3403ea01f143b9a6da1
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-05-06 23:45:33 -0300

    Fixed usings

--- Proxier/Extensions/TypeExtensions.cs
+++ Proxier/Extensions/TypeExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using FluentCache;
 using FluentCache.Simple;
 using Proxier.Contexts;
