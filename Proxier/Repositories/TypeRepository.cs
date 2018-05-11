d60433a900112c225060a5f672b1cf9885320422
Ronaldo Junior <ronaldo.ribeiro@outlook.com>
2018-05-11 09:53:03 -0300

    Fix minor bugs

--- Proxier/Repositories/TypeRepository.cs
+++ Proxier/Repositories/TypeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Proxier.Repositories
@@ -8,9 +9,11 @@ namespace Proxier.Repositories
         public PropertyInfo[] GetProperty(Type type, bool ignorePrivate)
         {
             return ignorePrivate
-                ? type.GetProperties()
+                ? type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(
+                    i => i.PropertyType.Namespace != null && i.PropertyType.Namespace.StartsWith("System") || i.PropertyType.IsEnum).ToArray()
                 : type
-                    .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                    .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Where(
+                        i => i.PropertyType.Namespace != null && i.PropertyType.Namespace.StartsWith("System") || i.PropertyType.IsEnum).ToArray();
         }
     }
 }
